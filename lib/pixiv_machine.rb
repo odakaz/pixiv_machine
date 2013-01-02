0b6eeac15ef1da62eed0f26f77a977a1f02cfe1e
odakaz <odakaz.g@gmail.com>
2013-01-02 14:26:20 +0900

    requireの仕方を修正

--- lib/pixiv_machine.rb
+++ lib/pixiv_machine.rb
@@ -21,9 +21,9 @@ end
 class PixivMachine::LoginError < PixivMachine::PixivError
 end
 
-require File.expand_path('./pixiv_machine/url')
-require File.expand_path('./pixiv_machine/page')
-require File.expand_path('./pixiv_machine/pageable_page')
-require File.expand_path('./pixiv_machine/my_page')
-require File.expand_path('./pixiv_machine/user_page')
-require File.expand_path('./pixiv_machine/illust_page')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/url')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/page')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/pageable_page')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/my_page')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/user_page')
+require File.expand_path(File.dirname(__FILE__) + '/pixiv_machine/illust_page')
