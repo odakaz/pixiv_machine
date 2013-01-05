9d177eff483bde82e6037e5837a6891b97e10896
odakaz <odakaz.g@gmail.com>
2013-01-06 01:13:20 +0900

    WebMockのスタブ設定に、作品一覧ページを追加

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -24,31 +24,45 @@ def setup_default_fixtures
               :headers => {'Content-Type' => 'text/html'},
               :body => File.new("#{FIXTURE_ROOT}/mypage"))
 
-  # 公開中お気に入りユーザーブックマークページ　１ページめ
+  # 公開中お気に入りユーザーブックマークページ　１ページ目
   WebMock.stub_request(:any, BOOKMARK_PATH).
     with(:query => {:type => 'user', :p => '1', :rest => 'show'}).
     to_return(:status => 200,
               :headers => {'Content-Type' => 'text/html'},
               :body => File.new("#{FIXTURE_ROOT}/shown_user_bookmark_page_1"))
 
-  # 公開中お気に入りユーザーブックマークページ　２ページめ
+  # 公開中お気に入りユーザーブックマークページ　２ページ目
   WebMock.stub_request(:any, BOOKMARK_PATH).
     with(:query => {:type => 'user', :p => '2', :rest => 'show'}).
     to_return(:status => 200,
               :headers => {'Content-Type' => 'text/html'},
               :body => File.new("#{FIXTURE_ROOT}/shown_user_bookmark_page_2"))
 
-  # 非公開中お気に入りユーザーブックマークページ　１ページめ
+  # 非公開中お気に入りユーザーブックマークページ　１ページ目
   WebMock.stub_request(:any, BOOKMARK_PATH).
     with(:query => {:type => 'user', :p => '1', :rest => 'hide'}).
     to_return(:status => 200,
               :headers => {'Content-Type' => 'text/html'},
               :body => File.new("#{FIXTURE_ROOT}/hidden_user_bookmark_page_1"))
 
-  # 非公開中お気に入りユーザーブックマークページ　２ページめ
+  # 非公開中お気に入りユーザーブックマークページ　２ページ目
   WebMock.stub_request(:any, BOOKMARK_PATH).
     with(:query => {:type => 'user', :p => '2', :rest => 'hide'}).
     to_return(:status => 200,
               :headers => {'Content-Type' => 'text/html'},
               :body => File.new("#{FIXTURE_ROOT}/hidden_user_bookmark_page_2"))
+
+  # 作品一覧ページ　１ページ目
+  WebMock.stub_request(:any, USER_ILLUST_PATH).
+    with(:query => {:id => '999', :p => 1}).
+    to_return(:status => 200,
+              :headers => {'Content-Type' => 'text/html'},
+              :body => File.new("#{FIXTURE_ROOT}/user_illust_page_1"))
+
+  # 作品一覧ページ　２ページ目
+  WebMock.stub_request(:any, USER_ILLUST_PATH).
+    with(:query => {:id => '999', :p => 2}).
+    to_return(:status => 200,
+              :headers => {'Content-Type' => 'text/html'},
+              :body => File.new("#{FIXTURE_ROOT}/user_illust_page_2"))
 end
