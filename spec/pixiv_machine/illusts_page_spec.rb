ca950083eaf8b819f26fcf9be1862322ef8b1ccf
odakaz <odakaz.g@gmail.com>
2013-01-11 22:06:38 +0900

    PixivMachine::IllustsPageのspecを手直し

--- spec/pixiv_machine/illusts_page_spec.rb
+++ spec/pixiv_machine/illusts_page_spec.rb
@@ -11,26 +11,26 @@ describe PixivMachine::IllustsPage do
 
   describe "#illusts" do
     context "ページ指定なし" do
-      it {
-        illusts = @page.illusts
-        WebMock.should have_requested(:get, USER_ILLUST_PATH).with(:query => {:id => '999', :p => '1'})
-        illusts.size.should eq 48
+      subject {@page.illusts}
+      its(:size) {should eq 48}
+      its(:first) {should be_an_instance_of PixivMachine::Content}
+      its(:last) {should be_an_instance_of PixivMachine::Content}
 
-        illusts.each {|i|
-          i.should be_an_instance_of PixivMachine::Content
-        }
+      it {
+        @page.illusts
+        WebMock.should have_requested(:get, USER_ILLUST_PATH).with(:query => {:id => '999', :p => '1'})
       }
     end
 
     context "ページ指定あり" do 
-      it {
-        illusts = @page.illusts 2
-        WebMock.should have_requested(:get, USER_ILLUST_PATH).with(:query => {:id => '999', :p => '2'})
-        illusts.size.should eq 10
+      subject {@page.illusts 2}
+      its(:size) {should eq 10}
+      its(:first) {should be_an_instance_of PixivMachine::Content}
+      its(:last) {should be_an_instance_of PixivMachine::Content}
 
-        illusts.each {|i|
-          i.should be_an_instance_of PixivMachine::Content
-        }
+      it {
+        @page.illusts 2
+        WebMock.should have_requested(:get, USER_ILLUST_PATH).with(:query => {:id => '999', :p => '2'})
       }
     end
   end
